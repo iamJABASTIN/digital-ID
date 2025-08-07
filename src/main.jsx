d546092e4207b60064d5df34c329452a6b9ddcbe
Jabastin A <itsmejabastin@gmail.com>
2025-08-07 13:46:42 +0530

    feat(Vercel analytics): add analytics feature for vercel dashboard

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
-
 import App from "./App.jsx";
 import "./index.css";
+import { Analytics } from "@vercel/analytics/react"; // Add this import
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
       <App />
     </Router>
+    <Analytics /> {/* Add this component */}
   </React.StrictMode>
 );
